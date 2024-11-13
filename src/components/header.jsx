fee89b19b5aaeb56423d159f4a9dbe34b78873ce
jjdavenport <jordanjamesdavenport@gmail.com>
2024-11-13 23:50:03 +0000

    fix: header padding

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,8 +6,8 @@ const Header = ({ score }) => {
       <header className="flex w-full justify-between p-4 md:w-9/12 lg:w-7/12">
         <div className="flex w-full justify-between rounded-lg outline outline-4 outline-headerOutline ~sm/md:~p-2/4 md:rounded-xl">
           <img src={logo} className="w-5/12 object-contain p-4 md:w-48" />
-          <div className="flex flex-col items-center justify-center rounded-md bg-white ~sm/md:~p-2/4">
-            <h1 className="uppercase tracking-widest text-scoreText ~sm/md:~text-base/2xl">
+          <div className="flex flex-col items-center justify-center rounded-lg bg-white ~sm/md:~px-6/12 ~sm/md:~py-2/4">
+            <h1 className="uppercase tracking-widest text-scoreText ~sm/md:~text-base/lg">
               Score
             </h1>
             <h2 className="font-bold text-darkText ~sm/md:~text-5xl/6xl">
